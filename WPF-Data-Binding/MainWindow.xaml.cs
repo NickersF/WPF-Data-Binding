0ce6c8dad59ff4709628e8d16d024f61ae32b7d3
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-05-30 21:25:14 -0700

    Added a basic data set to the project. Learning how to work with it.

--- WPF-Data-Binding/MainWindow.xaml.cs
+++ WPF-Data-Binding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Data_Binding.learningSQLDataSetTableAdapters;
 
 namespace WPF_Data_Binding
 {
@@ -23,6 +24,33 @@ namespace WPF_Data_Binding
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            learningSQLDataSet learningSQLDataSet = ((learningSQLDataSet)(this.FindResource("learningSQLDataSet")));
+            // Load data into the table employee. You can modify this code as needed.
+            employeeTableAdapter learningSQLDataSetemployeeTableAdapter = new employeeTableAdapter();
+            learningSQLDataSetemployeeTableAdapter.Fill(learningSQLDataSet.employee);
+            CollectionViewSource employeeViewSource = ((CollectionViewSource)(this.FindResource("employeeViewSource")));
+            employeeViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            employeeTableAdapter employeeTableAdapter = new employeeTableAdapter();
+            Random random = new Random();
+
+            int id = random.Next(0, 1000);
+            string firstName = firstNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            string email = emailTextBox.Text;
+
+            employeeTableAdapter.Insert(id, firstName, lastName, email);
+
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            emailTextBox.Text = "";
         }
     }
 }
---
+++ WPF-Data-Binding/learningSQLDataSet.cs
@@ -0,0 +1,16 @@
+﻿namespace WPF_Data_Binding
+{
+
+
+    partial class learningSQLDataSet
+    {
+    }
+}
+
+namespace WPF_Data_Binding.learningSQLDataSetTableAdapters {
+    
+    
+    public partial class employeeTableAdapter {
+        
+    }
+}
